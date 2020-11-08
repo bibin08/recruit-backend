89c858ca4b5b4f8e4def900a78bda26ee4f4a5c5
Bibin Varghese <bibin.varghese008@gmail.com>
2020-11-08 20:31:30 +1300

    Created framework for Integration test. Haven't finished the bindings due to time constraints

---
+++ API.CreditCard/API.CreditCard.IntegrationTests/Bindings/CreditCard_LISTSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace API.CreditCard.IntegrationTests.Bindings
+{
+    //TODO: Fill in the steps for Given, When and Then
+    [Binding]
+    public class CreditCard_LISTSteps
+    {
+        [Given(@"I have a HTTP client")]
+        public void GivenIHaveAHTTPClient()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I am testing SelfUrl")]
+        public void GivenIAmTestingSelfUrl()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I am testing v(.*) of Cards")]
+        public void GivenIAmTestingVOfCards(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I make a GET Request")]
+        public void WhenIMakeAGETRequest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the response status should be OK")]
+        public void ThenTheResponseStatusShouldBeOK()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ API.CreditCard/API.CreditCard.IntegrationTests/CreditCard_LIST.feature
@@ -0,0 +1,13 @@
+﻿Feature: CreditCard_LIST
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@CreditCard_v1
+Scenario: GetAll Credit Card
+	Given I have a HTTP client
+		And I am testing SelfUrl
+		And I am testing v1 of Cards
+	When I make a GET Request
+	Then the response status should be OK
+	
